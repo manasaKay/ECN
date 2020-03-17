fce44ae6a68345546b1c9dd1a985b348d8bf019a
ssreekri <ssreekri@eng.ucsd.edu>
2020-03-16 23:50:51 -0700

    Updated hyper-parameter tuning

--- mmd_coral_tuning.sh
+++ mmd_coral_tuning.sh
@@ -5,8 +5,10 @@ do
     for lmd_ext in $lmd_exts
     do
         echo $lmd, $lmd_ext
-        python3 main.py -s duke -t market -mmd 1 --lmd $lmd --lmd_ext $lmd_ext >"log_mmd_$lmd,$lmd_ext.txt" 2>error.txt
-        python3 main.py -s market -t duke -coral 1 --lmd $lmd --lmd_ext $lmd_ext >"log_coral_$lmd,$lmd_ext.txt" 2>error.txt
+        python3 main.py -s duke -t market -mmd 1 --lmd $lmd --lmd_ext $lmd_ext >"log_mmd_d_m_$lmd,$lmd_ext.txt" 2>error.txt
+        python3 main.py -s market -t duke -mmd 1 --lmd $lmd --lmd_ext $lmd_ext >"log_mmd_m_d_$lmd,$lmd_ext.txt" 2>error.txt
+        python3 main.py -s duke -t market -coral 1 --lmd $lmd --lmd_ext $lmd_ext >"log_coral_d_m_$lmd,$lmd_ext.txt" 2>error.txt
+        python3 main.py -s market -t duke -coral 1 --lmd $lmd --lmd_ext $lmd_ext >"log_coral_m_d_$lmd,$lmd_ext.txt" 2>error.txt
         rm logs/log.txt
     done
 done
