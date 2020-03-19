c70af269ead43450fbce1d8850c808b240cd9593
shreenivas <vshreenivasbharadwaj@gmail.com>
2020-03-18 19:14:17 -0700

    Results added

--- mmd_coral_tuning.py
+++ mmd_coral_tuning.py
@@ -1,6 +1,6 @@
 import os
 
-lmds = ['0.3']
+lmds = ['0', '0.3']
 lmd_exts = ['0.33', '0.66', '1']
 ignore = []
 
@@ -9,7 +9,7 @@ for lmd in lmds:
         if (lmd, lmd_ext) in ignore:
             continue
         print(lmd, lmd_ext)
-        os.system("python3 main.py -s duke -t market -mmd 1 --lmd %s --lmd_ext %s >log_mmd_d_m_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
-        os.system("python3 main.py -s market -t duke -mmd 1 --lmd %s --lmd_ext %s >log_mmd_m_d_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
-        os.system("python3 main.py -s duke -t market -coral 1 --lmd %s --lmd_ext %s >log_coral_d_m_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
-        os.system("python3 main.py -s market -t duke -coral 1 --lmd %s --lmd_ext %s >log_coral_m_d_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
+        os.system("python3 main.py -s duke -t market -cs cyclegan -mmd 1 --lmd %s --lmd_ext %s >log_mmd_d_m_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
+        os.system("python3 main.py -s market -t duke -cs cyclegan -mmd 1 --lmd %s --lmd_ext %s >log_mmd_m_d_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
+        os.system("python3 main.py -s duke -t market -cs cyclegan -coral 1 --lmd %s --lmd_ext %s >log_coral_d_m_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
+        os.system("python3 main.py -s market -t duke -cs cyclegan -coral 1 --lmd %s --lmd_ext %s >log_coral_m_d_%s,%s.txt 2>error.txt" % (lmd, lmd_ext, lmd, lmd_ext))
